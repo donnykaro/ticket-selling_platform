8b9933cafd0f69f7267eef7a95126ba49a3ef6fe
Karol Bekieszczuk <karol.bekieszczuk@gmail.com>
2019-06-06 13:07:20 +0200

    fixed migrations

--- db/migrate/20190519200642_add_status_to_order.rb
+++ db/migrate/20190519200642_add_status_to_order.rb
@@ -1,5 +1,5 @@
 class AddStatusToOrder < ActiveRecord::Migration[5.2]
   def change
-  	add_column(:orders, :status, :string)
+  	add_column(:orders, :state, :string)
   end
 end
--- db/migrate/20190522140620_add_default_to_order_status.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultToOrderStatus < ActiveRecord::Migration[5.2]
-  def change
-  	change_column :orders, :status, :string, default: "reserved"
-  end
-end
